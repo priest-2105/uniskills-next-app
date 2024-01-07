9a4782d465918d7c4ad253c0b0218b4cfacf957f
priest-2105 <fawzybailey782@gmail.com>
2024-01-07 22:42:01 +0100

    gi

--- pages/helpers/app_utilities.js
+++ pages/helpers/app_utilities.js
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 
 // Custom hook to perform some utility logic
-export function PRELOADER_CONTROLLER() {
+export function usePreloaderController() {
   useEffect(() => {
     const removePreloader = () => {
       const preloader = document.querySelector('.cs-page-loading');
