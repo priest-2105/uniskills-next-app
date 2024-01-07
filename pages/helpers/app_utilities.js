18820627ba82b763b4f28bfad6381e935ca9d9f7
priest-2105 <fawzybailey782@gmail.com>
2024-01-07 21:50:50 +0100

    gi

--- pages/helpers/app_utilities.js
+++ pages/helpers/app_utilities.js
@@ -1,22 +1,8 @@
-
-
-
-
-// // FUNCTION TO CONTROLLER THE PRELOADER SHOWING EACH TIME THE PAGE LOADS FOR THE FIRST TIME
-// export function PRELOADER_CONTROLLER(){
-//     var preloader = document.querySelector('.cs-page-loading');
-//     if(preloader){
-//         preloader.classList.remove('active');
-//         setTimeout(function () {
-//             preloader.remove();
-//         }, 2000);
-//     }
-// };
-
+// app_utilities.js
 import { useEffect } from 'react';
 
-// Function to control the preloader
-export function PRELOADER_CONTROLLER() {
+// Custom hook to perform some utility logic
+export function usePreloaderController() {
   useEffect(() => {
     const removePreloader = () => {
       const preloader = document.querySelector('.cs-page-loading');
@@ -31,9 +17,9 @@ export function PRELOADER_CONTROLLER() {
     // Call the function when the component mounts
     removePreloader();
 
-    // Cleanup the function on component unmount
+    // Cleanup (if needed)
     return () => {
-      // You can perform cleanup tasks if needed
+      // Cleanup tasks (if any)
     };
   }, []); // Empty dependency array ensures this effect runs once on mount
 }
